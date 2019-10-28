8f52c3720964660c50d0ff5cd2ed0a8e1b67ff2f
wgrimshaw <walker.grimshaw@duke.edu>
2019-10-28 12:53:37 -0400

    Update ImportARGOS.py

--- ImportARGOS.py
+++ ImportARGOS.py
@@ -15,9 +15,10 @@ import sys, os, arcpy
 arcpy.env.overwriteOutput = True
 
 # Set input variables (Hard-wired)
-inputFile = '../Data/ARGOSData/1997dg.txt'
-outputFC = '../Scratch/ARGOStrack.shp'
-outputSR = arcpy.SpatialReference(54002)
+inputFolder = arcpy.GetParameterAsText(0) #'../Data/ARGOSData'
+inputFiles = os.listdir(inputFolder)
+outputFC = arcpy.GetParameterAsText(1) #'../Scratch/ARGOStrack.shp'
+outputSR = arcpy.GetParameterAsText(2) #arcpy.SpatialReference(54002)
 
 ## Prepare a new feature class to which we'll add tracking points
 # Create an empty feature class; requires the path and name as separate parameters
@@ -33,72 +34,81 @@ arcpy.AddField_management(outputFC,"Date","DATE")
 cur = arcpy.da.InsertCursor(outputFC,['Shape@','TagID','LC','Date'])
 
 ## Construct a while loop to iterate through all lines in the datafile
-# Open the ARGOS data file for reading
-inputFileObj = open(inputFile,'r')
-
-# Get the first line of data, so we can use a while loop
-lineString = inputFileObj.readline()
-while lineString:
+for inputFile in inputFiles:
+    ## note to update user
+    print("Processing {}".format(inputFile))
+    arcpy.AddMessage("Processing {}".format(inputFile))
+    ## Skip readme.txt
+    if inputFile == 'readme.txt':
+        continue
+    ## need to provide file path before opening file
+    inputFilePath = os.path.join(inputFolder, inputFile)
+    # Open the ARGOS data file for reading
+    inputFileObj = open(inputFilePath,'r')
     
-    # Set code to run only if the line contains the string "Date: "
-    if ("Date :" in lineString):
-        
-        # Parse the line into a list
-        lineData = lineString.split()
-        
-        # Extract attributes from the datum header line
-        tagID = lineData[0]
-        obsDate = lineData[3]
-        obsTime = lineData[4]
-        obsLC = lineData[7]
-        
-        # Extract location info from the next line
-        line2String = inputFileObj.readline()
-        
-        # Parse the line into a list
-        line2Data = line2String.split()
-        
-        # Extract the date we need to variables
-        obsLat = line2Data[2]
-        obsLon= line2Data[5]
-
-        # Print results to see how we're doing
-        #print (tagID,obsDate,obsTime,obsLC,"Lat:"+obsLat,"Long:"+obsLon)
-
-        #Try to convert the coordinates to numbers
-        try:
-
-            # Convert raw coordinate strings to numbers
-            if obsLat[-1] == 'N':
-                obsLat = float(obsLat[:-1])
-            else:
-                obsLat = float(obsLat[:-1]) * -1
-            if obsLon[-1] == 'E':
-                obsLon = float(obsLon[:-1])
-            else:
-                obsLon = float(obsLon[:-1]) * -1
-               
-            # Construct a point object from the feature class
-            obsPoint = arcpy.Point()
-            obsPoint.X = obsLon
-            obsPoint.Y = obsLat
-            
-            # Convert the point to a point geometry object with spatial reference
-            inputSR = arcpy.SpatialReference(4326)
-            obsPointGeom = arcpy.PointGeometry(obsPoint,inputSR)
-
-            # Create a feature object
-            cur.insertRow((obsPointGeom,tagID,obsLC,obsDate.replace(".","/") + " " + obsTime))
-
-        #Handle any error
-        except Exception as e:
-            print("Error adding record {} to the output".format(tagID))
-
-    # Move to the next line so the while loop progresses
+    # Get the first line of data, so we can use a while loop
     lineString = inputFileObj.readline()
+    while lineString:
+        
+        # Set code to run only if the line contains the string "Date: "
+        if ("Date :" in lineString):
+            
+            # Parse the line into a list
+            lineData = lineString.split()
+            
+            # Extract attributes from the datum header line
+            tagID = lineData[0]
+            obsDate = lineData[3]
+            obsTime = lineData[4]
+            obsLC = lineData[7]
+            
+            # Extract location info from the next line
+            line2String = inputFileObj.readline()
+            
+            # Parse the line into a list
+            line2Data = line2String.split()
+            
+            # Extract the date we need to variables
+            obsLat = line2Data[2]
+            obsLon= line2Data[5]
     
-#Close the file object
-inputFileObj.close()
+            # Print results to see how we're doing
+            #print (tagID,obsDate,obsTime,obsLC,"Lat:"+obsLat,"Long:"+obsLon)
+    
+            #Try to convert the coordinates to numbers
+            try:
+    
+                # Convert raw coordinate strings to numbers
+                if obsLat[-1] == 'N':
+                    obsLat = float(obsLat[:-1])
+                else:
+                    obsLat = float(obsLat[:-1]) * -1
+                if obsLon[-1] == 'E':
+                    obsLon = float(obsLon[:-1])
+                else:
+                    obsLon = float(obsLon[:-1]) * -1
+                   
+                # Construct a point object from the feature class
+                obsPoint = arcpy.Point()
+                obsPoint.X = obsLon
+                obsPoint.Y = obsLat
+                
+                # Convert the point to a point geometry object with spatial reference
+                inputSR = arcpy.SpatialReference(4326)
+                obsPointGeom = arcpy.PointGeometry(obsPoint,inputSR)
+    
+                # Create a feature object
+                cur.insertRow((obsPointGeom,tagID,obsLC,obsDate.replace(".","/") + " " + obsTime))
+    
+            #Handle any error
+            except Exception as e:
+                print("Error adding record {} to the output".format(tagID))
+    
+        # Move to the next line so the while loop progresses
+        lineString = inputFileObj.readline()
+        
+    #Close the file object
+    inputFileObj.close()
 
 #Delete the cursor object
 del cur
